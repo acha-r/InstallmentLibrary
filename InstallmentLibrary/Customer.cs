d7b41b14d7b931b35278ccec20d9e749e5c216bb
Rachel Okorie <jayrachel96@gmail.com>
2023-01-16 09:26:17 +0100

    Add project files.

---
+++ InstallmentLibrary/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstallmentLibrary
+{
+    public abstract class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double AmountToPay { get; set; }
+
+        protected PaymentStatusEnum Status = PaymentStatusEnum.IncompletePayment;
+        public PaymentStatusEnum PaymentStatus => Status;
+
+        protected Customer() { }
+
+        protected Customer(string name, double amountToPay)        
+        {
+            Name = name;
+            AmountToPay = amountToPay;
+        }
+
+    }
+}
