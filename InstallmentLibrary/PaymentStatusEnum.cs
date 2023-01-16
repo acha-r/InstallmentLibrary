7de7291f8a7ab7bafc4987e3b8008e8c95eb7b9c
Rachel Okorie <jayrachel96@gmail.com>
2023-01-16 09:30:59 +0100

    Renamed solution and created a repository

--- InstallmentLibrary/PaymentStatusEnum.cs
+++ InstallmentLibrary/PaymentStatusEnum.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InstallmentLibrary
+namespace InstallmentPaymentLibrary
 {
     public enum PaymentStatusEnum
     {
